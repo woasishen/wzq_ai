9a8479330ca5a94fc0274029694961381c96e2d0
woasishen <76015064@qq.com>
2016-12-26 21:08:31 +0800

    f

---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        public static readonly Dictionary<int, int> GOLE_DICT = new Dictionary<int, int>
+            {
+                {1,1 },
+                {2,100 },
+                {3,10000 },
+                {4,1000000 },
+                {5,100000000 }
+            };
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(-GOLE_DICT[5]);
+        }
+    }
+}
