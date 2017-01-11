e16bfcbc2f604aefbe127edb96e28a302f85e4e0
woasishen <76015064@qq.com>
2017-01-11 19:19:31 +0800

    search range

--- wzq_ai/MaxMin.cs
+++ wzq_ai/MaxMin.cs
@@ -9,6 +9,8 @@ namespace wzq_ai
     {
         private readonly Evaluate evaluate;
         private readonly CellStatus[][] cellStatusArr;
+        private readonly int width;
+        private readonly int height;
         private int recusionTimes;
 
         public Action<TimeSpan, int> ComputeFinish;
@@ -19,13 +21,41 @@ namespace wzq_ai
         {
             cellStatusArr = curStatusArr;
             evaluate = new Evaluate(cellStatusArr, width, height);
+            this.width = width;
+            this.height = height;
         }
 
         public GolePos GeneBestPos(CellStatus cellStatus)
         {
             var startTime = DateTime.Now;
             recusionTimes = 0;
-            var result = GeneBestPos(cellStatus, 2);
+
+            int xMax, xMin, yMax, yMin;
+            xMin = yMin = int.MaxValue;
+            xMax = yMax = int.MinValue;
+            bool emptyCellArr = true;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (cellStatusArr[i][j] != CellStatus.Empty)
+                    {
+                        emptyCellArr = false;
+                        xMax = Math.Min(Math.Max(xMax, i + 1), width);
+                        xMin = Math.Max(Math.Min(xMin, i - 1), 0);
+                        yMax = Math.Min(Math.Max(yMax, j + 1), height);
+                        yMin = Math.Max(Math.Min(yMin, j - 1), 0);
+                    }
+                }
+            }
+            if (emptyCellArr)
+            {
+                var tempStack = new Stack<Pos>();
+                tempStack.Push(new Pos(width/2, height/2));
+                return new GolePos(0, tempStack);
+            }
+
+            var result = GeneBestPos(cellStatus, 2, xMax, xMin, yMax, yMin);
             ComputeFinish.Invoke(startTime - DateTime.Now, recusionTimes);
             return result;
         }
@@ -35,16 +65,21 @@ namespace wzq_ai
         /// </summary>
         /// <param name="cellStatus">当前走棋方</param>
         /// <param name="depth">计算深度</param>
+        /// <param name="xMax"></param>
+        /// <param name="xMin"></param>
+        /// <param name="yMax"></param>
+        /// <param name="yMin"></param>
         /// <returns></returns>
-        private GolePos GeneBestPos(CellStatus cellStatus, int depth)
+        private GolePos GeneBestPos(CellStatus cellStatus, int depth, 
+            int xMax, int xMin, int yMax, int yMin)
         {
             recusionTimes++;
             var oldSelfGole = evaluate.ComputeGole(cellStatus);
             var oldOtherGole = evaluate.ComputeGole(CellStatusHelper.Not(cellStatus));
             var tempGoleList = new Dictionary<int, Stack<Pos>>();
-            for (int x = 0; x < cellStatusArr.Length; x++)
+            for (int x = xMin; x < xMax; x++)
             {
-                for (int y = 0; y < cellStatusArr[x].Length; y++)
+                for (int y = yMin; y < yMax; y++)
                 {
                     if (!ShouldCompute(x, y))
                     {
@@ -79,7 +114,13 @@ namespace wzq_ai
                     }
                     else
                     {
-                        var tempGolePos = GeneBestPos(CellStatusHelper.Not(cellStatus), depth - 1);
+                        var tempGolePos = GeneBestPos(
+                            CellStatusHelper.Not(cellStatus),
+                            depth - 1,
+                            Math.Min(Math.Max(xMax, x + 1), width),
+                            Math.Max(Math.Min(xMin, x - 1), 0),
+                            Math.Min(Math.Max(yMax, y + 1), height),
+                            Math.Max(Math.Min(yMin, y - 1), 0));
                         tempGolePos.PosStack.Push(tempPos);
                         tempGoleList[tempGolePos.Gole] = tempGolePos.PosStack;
                     }
